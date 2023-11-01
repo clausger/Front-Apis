85e24c5592c0036988cda30a7170fd276bb8afcf
unknown <juanchicanestrari@gmail.com>
2023-11-01 16:05:31 -0300

    Agrege el login y home para poder probarlo

--- src/ReclamosApp.jsx
+++ src/ReclamosApp.jsx
@@ -1,9 +1,11 @@
 import { Home } from "./Componentes/Home"
+import { Login } from "./Componentes/Login"
 
 export const ReclamosApp = () => {
   return (
     <div>
-    
+      <Login></Login>
+      <Home></Home>
     
     
     </div>
