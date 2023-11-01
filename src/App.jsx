f270d184a11d09ab062e9f0a1d728c07a5acce64
Bautista <bautista.janse@gmail.com>
2023-10-31 21:01:34 -0300

    instale el router dom

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-
-export const App = () => {
-  return (
-    <div><h1>App andando</h1></div>
-  )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { Login } from './Componentes/Login'
+import { ReclamosApp } from './ReclamosApp'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  
   <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
+
+    <ReclamosApp />
+      
+  </React.StrictMode>
+
+ 
 )
