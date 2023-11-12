83f1158f4a5a7bb2254231ea3e24f3be229b4299
unknown <juanchicanestrari@gmail.com>
2023-11-12 01:56:11 -0300

    FEAT (AgregarUsuario) Agrege el componente de agregar usuario

--- src/Componentes/usuarios/VerUsuarios.jsx
+++ src/Componentes/usuarios/VerUsuarios.jsx
@@ -1,4 +1,7 @@
 export const VerUsuarios = () => {
+
+    // ACA VA UN TABLA PARA USUARIOS
+    // TODOS TIENE TELELFONO, DIRECCION, EMAIL, NOMBRE, NOMBRE_US
     return (
         <div>VerUsuarios</div>
     )
