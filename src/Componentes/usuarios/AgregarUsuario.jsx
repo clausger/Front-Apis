87c4eea6fb17575189c452921ae77febeb376b1c
unknown <juanchicanestrari@gmail.com>
2023-11-12 01:54:58 -0300

    FEAT (AgregarUsuario) Agrege el componente de agregar usuario

---
+++ src/Componentes/usuarios/AgregarUsuario.jsx
@@ -0,0 +1,8 @@
+export const AgregarUsuario = () => {
+
+    // ACA VA UN TABLA PARA USUARIOS
+    // TODOS TIENE TELELFONO, DIRECCION, EMAIL, NOMBRE, NOMBRE_US
+    return (
+        <div>AgregarUsuario</div>
+    )
+}
