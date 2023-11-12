55eea82e246ed866f7fd218ed768dfa617063e85
unknown <juanchicanestrari@gmail.com>
2023-11-12 10:50:55 -0300

    FIX (VerUsuarios) Agrege la tabla con los atributos y los botones para volver y agregar linkeados

--- src/Componentes/usuarios/VerUsuarios.jsx
+++ src/Componentes/usuarios/VerUsuarios.jsx
@@ -1,8 +1,56 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+
 export const VerUsuarios = () => {
 
     // ACA VA UN TABLA PARA USUARIOS
     // TODOS TIENE TELELFONO, DIRECCION, EMAIL, NOMBRE, NOMBRE_US
+
+    const [usuarios, setUsuario] = useState([{"nombre":"cambio","nombreUs":"admin","telefono":1,"email":"email@prueba","direcion":"pruebaDir"}]);
+
+    // useEffect(() => {
+    //     obtenerUsuarios();
+    // },[]);
+
     return (
-        <div>VerUsuarios</div>
+        <div>
+            <div>
+                <Link to="/Home">
+                    <button>Back</button>
+                </Link>
+                
+                <Link to="/Usuarios/Agregar">
+                    <button>AgregarUsuario</button>
+                </Link>
+
+            </div>
+            <div className="bodyUsuarios">
+                <h2>Ver Usuarios</h2>
+                <table className="tablaUsuarios">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Usuario</th>
+                            <th>Email</th>
+                            <th>Direcion</th>
+                            <th>Telefono</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usuarios.map(usuario => (
+                            <tr key={usuario.id}>
+                                <td>{usuario.nombre}</td>
+                                <td>{usuario.nombreUs}</td>
+                                <td>{usuario.email}</td>
+                                <td>{usuario.direcion}</td>
+                                <td>{usuario.telefono}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
     )
 }
--- src/ReclamosApp.jsx
+++ src/ReclamosApp.jsx
@@ -2,6 +2,7 @@ import { Home } from "./Componentes/home/Home"
 import { Login } from "./Componentes/login/Login"
 import { Route, Routes } from "react-router-dom"
 import { VerUsuarios } from "./Componentes/usuarios/VerUsuarios"
+import { AgregarUsuario } from "./Componentes/usuarios/AgregarUsuario"
 
 export const ReclamosApp = () => { 
   return (
@@ -10,6 +11,7 @@ export const ReclamosApp = () => {
         <Route path="/" element={<Login> </Login>}> </Route>
         <Route path="/Home" element={<Home> </Home>}> </Route>
         <Route path="/Usuarios" element={<VerUsuarios></VerUsuarios>}></Route>
+        <Route path="/Usuarios/Agregar" element={<AgregarUsuario></AgregarUsuario>}></Route>
       </Routes>
     </div>
   )
