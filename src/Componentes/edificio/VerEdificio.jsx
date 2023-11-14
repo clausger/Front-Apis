bb52d8a911c81a3e4432038293382cb8fcf76512
unknown <juanchicanestrari@gmail.com>
2023-11-14 01:28:55 -0300

    FEAT (VerEdificio) Agregamos la conexion a la api

--- src/Componentes/edificio/VerEdificio.jsx
+++ src/Componentes/edificio/VerEdificio.jsx
@@ -1,10 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const VerEdificio = () => {
     
     const [edificios, setEdificios] = useState([])
 
+    var token;
+
+    useEffect(() => {
+        token = sessionStorage.getItem("jwt")
+        if(token === undefined){
+            console.log(ERRROR)
+        }
+        obtenerEdificios();
+    },[])
+
+    const obtenerEdificios = async () =>{
+        const settings = {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            }
+        }
+
+        await fetch('http://localhost:8080/api/edificios', settings)
+        .then((response) =>{
+            if(!response.ok)
+                console.log("algo salio mal")
+            return response.json()
+        })
+        .then((data) => {
+            setEdificios(data)
+        }).catch((error) => {
+            console.log("ERROR")
+        })
+    }
 
     return ( 
     <div>
@@ -34,7 +65,7 @@ const VerEdificio = () => {
                                 <td>{edificio.codigoPostal}</td>
                                 <td>{edificio.pais}</td>
                                 <td>{<button>Actualizar</button>}</td>
-                                <td>{<button onClick={nothing}>Eliminar</button>}</td>
+                                <td>{<button /* onClick={nothing} */>Eliminar</button>}</td>
                             </tr>
                         ))}
                     </tbody>
