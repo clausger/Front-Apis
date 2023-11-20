8b1d9ba102a2b99347f00c8230922e877d9317de
clausger <148835433+clausger@users.noreply.github.com>
2023-11-20 03:48:34 +0000

    cambios css en login etc.

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { ReclamosApp } from './ReclamosApp'
 import { BrowserRouter } from 'react-router-dom'
 import VerEdificios from './Componentes/edificio/VerEdificio'
+import {Login} from './Componentes/login/Login.jsx'
+import {VerUsuarios} from './Componentes/usuarios/VerUsuarios.jsx'
+import {VerReclamosID} from './Componentes/reclamos/verReclamoID/VerReclamoID.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-
-      
-      <ReclamosApp></ReclamosApp>
+      {/* <Login></Login>  */}
+      {/*  <VerUsuarios></VerUsuarios>  */}
+      {/*  <ReclamosApp></ReclamosApp>*/}
+      <VerReclamosID></VerReclamosID>
 
     </React.StrictMode>
   </BrowserRouter>
